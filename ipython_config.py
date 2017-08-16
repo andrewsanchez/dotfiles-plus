f63ee7aacbed8a25174eaa66cdbb67261a4005b2
andrewsanchez <inbox.asanchez@gmail.com>
2017-08-16 01:02:09 -0700

    ipython config and screenrc

--- ipython_config.py
+++ ipython_config.py
@@ -31,8 +31,9 @@
 ## lines of code to run at IPython startup.
 c.InteractiveShellApp.exec_lines = [
         'from importlib import reload',
-        'import pandas as pd'
-        'import os',
+        'import pandas as pd',
+	'import numpy as np',
+        'import os'
         ]
 
 ## A list of dotted module names of IPython extensions to load.
