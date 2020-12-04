b3d26d1561d1bc43b6b2a88711b29fab201d8296
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-12-04 15:45:12 -0700

    Start server.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -303,6 +303,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (server-start)
   )
 
 (defun dotspacemacs/user-config ()
