712ba800b8a7db913424462d4f0721dc5e6ce4b6
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-03-08 14:40:29 -0700

    pip alias.

--- .zsh_custom/custom.zsh
+++ .zsh_custom/custom.zsh
@@ -6,3 +6,6 @@ alias i="ipython"
 alias sha_currentbranch="git rev-parse --short $(git rev-parse --abbrev-ref HEAD)"
 alias sha_prod="git rev-parse --short production"
 alias hvflake8="git diff ${sha_prod} ${sha_currentbranch} | flake8 --diff"
+
+# https://adamj.eu/tech/2020/02/25/use-python-m-pip-everywhere/
+alias pip='python -m pip'
