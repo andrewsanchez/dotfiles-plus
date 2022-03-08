441c5907a8970dbc0d13956c82dddcbc9931a5d2
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-03-08 14:39:15 -0700

    Aliases for running flake8 on diff.

--- .zsh_custom/custom.zsh
+++ .zsh_custom/custom.zsh
@@ -1,3 +1,8 @@
 function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}
 
 alias i="ipython"
+
+# Run
+alias sha_currentbranch="git rev-parse --short $(git rev-parse --abbrev-ref HEAD)"
+alias sha_prod="git rev-parse --short production"
+alias hvflake8="git diff ${sha_prod} ${sha_currentbranch} | flake8 --diff"
